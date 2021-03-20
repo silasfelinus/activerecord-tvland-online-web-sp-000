98b3800c5c43a21caa676270430e06c167f7845a
Silas Michael Knight <silasfelinus@gmail.com>
2021-03-20 06:02:03 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def actors_list
+    self.characters.collect {|character| "#{character.actor.first_name} #{character.actor.last_name}"}
+  end
+
+
+
 end
